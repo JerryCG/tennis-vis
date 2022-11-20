0819189e184979dd7c684e79f5894d038bc5f90a
JerryCG <jerryguo000111@gmail.com>
2022-11-19 20:22:46 -0600

    add dynamic gs counts
    process data about gs counts, select dominant players, and plot with flourish

--- pages/dynamics.py
+++ pages/dynamics.py
@@ -4,15 +4,32 @@ from dash import html
 dash.register_page(__name__, name = 'Dynamics')
 
 layout = html.Div(children = [
-        html.H2('Dynamic ATP Top10 from 2000 to 2022', style = {'padding-left': '10px'}),
-        html.Iframe(
-            src='https://flo.uri.sh/visualisation/11862538/embed',
-            title='Interactive or visual content',
-            className='flourish-embed-iframe',
-            style={'width':'50%','height':'500px', 'min-width': '500px', 'margin-left': '10px'}, 
-            sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation',
+    html.Div(children=[
+        html.Div(children=[
+            html.H2('Dynamic ATP Top10 from 2000 to 2022'),
+            html.Iframe(
+                src='https://flo.uri.sh/visualisation/11862538/embed',
+                title='Interactive or visual content',
+                className='flourish-embed-iframe',
+                style={'height':'500px', 'width': '90%'}, 
+                sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation',
+            ),
+            ],className='column2'
+            ),
+        html.Div(children=[
+            html.H2('GS Counts of Big ATP Players from 1990 to 2022'),
+            html.Iframe(
+                src='https://flo.uri.sh/visualisation/11883754/embed',
+                title='Interactive or visual content',
+                className='flourish-embed-iframe',
+                style={'width': '90%', 'height':'500px'}, 
+                sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation',
+            ),
+            ],className='column2'
+            ),
+        ], className='row2'
         ),
         html.Br(),
-        html.Br()
+        html.Br(),
     ])
 
