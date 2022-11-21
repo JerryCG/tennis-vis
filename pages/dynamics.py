b4c71af7bfc9e3e85c8defd0b6947bd31d1419e3
JerryCG <jerryguo000111@gmail.com>
2022-11-20 23:03:11 -0600

    add more player data

--- pages/dynamics.py
+++ pages/dynamics.py
@@ -14,8 +14,8 @@ layout = html.Div(children = [
                 style={'height':'500px', 'width': '90%'}, 
                 sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation',
             ),
-            ],className='column2'
-            ),
+        ],className='column2'
+        ),
         html.Div(children=[
             html.H2('GS Counts of Big ATP Players from 1990 to 2022'),
             html.Iframe(
@@ -25,8 +25,8 @@ layout = html.Div(children = [
                 style={'width': '90%', 'height':'500px'}, 
                 sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation',
             ),
-            ],className='column2'
-            ),
+        ],className='column2'
+        ),
     ], className='row2'
     ),
     html.Br(),
