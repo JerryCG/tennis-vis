7bfcc30d851a9fc0d4eeb07c1eccac84ce4a964a
JerryCG <jerryguo000111@gmail.com>
2023-01-17 08:54:56 -0600

    2023-01-16 data update

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ app.layout = html.Div([
             ]
             ),
             html.Div(
-                "Last Update: 2023-01-09", style = {'font-size': '20px', 'font-style': 'italic'}
+                "Last Update: 2023-01-16", style = {'font-size': '20px', 'font-style': 'italic'}
             ),
         ], style = {'text-align': 'center'}
     ),
