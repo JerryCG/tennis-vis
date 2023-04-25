1f96dccbaf8edef13e6e24e6859bdadb3c2f34f0
JerryCG <jerryguo000111@gmail.com>
2023-04-25 16:15:16 -0500

    2023-04-24

--- pages/geotennis.py
+++ pages/geotennis.py
@@ -11,7 +11,7 @@ dash.register_page(__name__, name = 'GeoTennis')
 layout = html.Div(children=[
     # section display a given ATP Ranking Date's Top 100 Players geographic distribution
     html.Div(children=[
-        html.H2('ATP Top100 Geographical Distribution from 2000-01-10 to 2023-04-17'),
+        html.H2('ATP Top100 Geographical Distribution from 2000-01-10 to 2023-04-24'),
         html.Iframe(
             src='https://flo.uri.sh/visualisation/11920998/embed',
             title='Interactive or visual content',
