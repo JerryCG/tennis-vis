70cf9552c51566c520b38ab512ec3f89c58cad94
JerryCG <jerryguo000111@gmail.com>
2025-04-15 09:01:41 +0800

    update some dates

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app.layout = html.Div([
             ]
             ),
             html.Div(
-                "Last Update: 2025-04-07", style = {'font-size': '20px', 'font-style': 'italic'}
+                "Last Update: 2025-04-14", style = {'font-size': '20px', 'font-style': 'italic'}
             ),
         ], style = {'text-align': 'center'}
     ),
