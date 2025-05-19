c843a777a7faefa470b4692a62bf5ab61daea28c
JerryCG <jerryguo000111@gmail.com>
2025-05-19 19:08:22 +0800

    2025-05-19 rank update

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app.layout = html.Div([
             ]
             ),
             html.Div(
-                "Last Update: 2025-05-05", style = {'font-size': '20px', 'font-style': 'italic'}
+                "Last Update: 2025-05-19", style = {'font-size': '20px', 'font-style': 'italic'}
             ),
         ], style = {'text-align': 'center'}
     ),
