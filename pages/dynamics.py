d105fbbdb7747d9645f9e68140a295b5f030dd89
JerryCG <jerryguo000111@gmail.com>
2025-08-13 15:00:05 +0800

    2025-08-04 rank update

--- pages/dynamics.py
+++ pages/dynamics.py
@@ -7,7 +7,7 @@ dash.register_page(__name__, name = 'Dynamics')
 layout = html.Div(children = [
     html.Div(children=[
         html.Div(children=[
-            html.H2('Dynamic ATP Top10 from 2000-01-10 to 2025-07-28'),
+            html.H2('Dynamic ATP Top10 from 2000-01-10 to 2025-08-04'),
             html.Iframe(
                 src='https://flo.uri.sh/visualisation/11862538/embed',
                 title='Interactive or visual content',
